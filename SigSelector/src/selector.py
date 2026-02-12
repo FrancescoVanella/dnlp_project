dc6e2abc7dc1604f34461790adc8e692bdda84a7
FrancescoVanella <162358212+FrancescoVanella@users.noreply.github.com>
2026-02-12 21:57:28 +0100

    Fix logic of selector.py

--- SigSelector/src/selector.py
+++ SigSelector/src/selector.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class DocumentSelector:
     """
@@ -26,30 +27,48 @@ class DocumentSelector:
                 'start': start,
                 'end': end,
                 'score': 0.0,
-                'num_phrases': 0
+                'num_scored_phrases': 0,
+                'total_phrases': 0 
             })
             current_idx = end + delimiter_len
 
         score_map = {item['kw_index']: item['score'] for item in scores_info}
 
         for idx, p_info in enumerate(phrases_info):
-            if idx not in score_map:
-                continue
-            p_score = score_map[idx]
             p_start = p_info['index']
-
+            
+            # Identify which document this phrase belongs to
+            target_doc = None
             for doc in doc_spans:
                 if doc['start'] <= p_start < doc['end']:
-                    doc['score'] += p_score
-                    doc['num_phrases'] += 1
+                    target_doc = doc
                     break
+            
+            if target_doc:
+                # Increment total phrases count for this doc
+                target_doc['total_phrases'] += 1
+                
+                # If the phrase has a score, add it
+                if idx in score_map:
+                    p_score = score_map[idx]
+                    # Convert log-prob to probability 
+                    term_score = np.exp(p_score)
+                    target_doc['score'] += term_score
+                    target_doc['num_scored_phrases'] += 1
 
         return doc_spans
 
     def select_top_k(self, data, k=2):
         scored_docs = self.score_documents(data)
+        
+        # Normalize scores by the total number of phrases in each document
+        for doc in scored_docs:
+            if doc['total_phrases'] > 0:
+                doc['score'] = doc['score'] / doc['total_phrases']
+            else:
+                doc['score'] = 0.0
+                
         scored_docs.sort(key=lambda x: x['score'], reverse=True)
         selected = scored_docs[:k]
         final_text = "\n".join([d['text'].strip() for d in selected])
-        
         return final_text, scored_docs
